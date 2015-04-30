050a3b5fe170dc6f7b18d31645c341a78e86ace9
Amit Jain <gravicle@gmail.com>
2015-04-30 15:50:58 -0500

    Commented code.

--- Transition/ViewController.swift
+++ Transition/ViewController.swift
@@ -25,11 +25,11 @@ class ViewController: UIViewController {
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
     
         // this gets a reference to the screen that we're about to transition to
-        let toViewController = segue.destinationViewController as! UIViewController
+//        let toViewController = segue.destinationViewController as! UIViewController
         
         // instead of using the default transition animation, we'll ask
         // the segue to use our custom TransitionManager object to manage the transition animation
-        toViewController.transitioningDelegate = self.transitionManager
+//        toViewController.transitioningDelegate = self.transitionManager
         
     }
     
